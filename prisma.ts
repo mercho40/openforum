f4c7d9450c234773d2407d669b8f8cdd058cd0b3
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-05-13 22:07:30 -0300

    fix prisma.ts

--- prisma.ts
+++ prisma.ts
@@ -1,10 +1,10 @@
 // import { PrismaClient } from '@prisma/client';
 import { PrismaClient } from "@/generated/prisma";
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
+// import { neonConfig } from '@neondatabase/serverless';
 
-import ws from 'ws';
-neonConfig.webSocketConstructor = ws;
+// import ws from 'ws';
+// neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
