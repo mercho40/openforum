0afd3a8b09bcf38ca6f6fcdc128264c6da5ebb78
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-05-26 17:11:08 -0300

    fix transaction drizzle error

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,8 +1,14 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
-import * as schema from './schema';
-config({ path: ".env" });
-
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql, schema });
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import { config } from "dotenv";
+//
+// config({ path: ".env" });
+//
+// const sql = neon(process.env.DATABASE_URL!);
+// export const db = drizzle({ client: sql });
+//
+import { Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import * as schema from "./schema";
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle({ client: pool, schema: schema })
