90d2af6185ffd9bfc0895c7833d75ae4f31fe70b
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-06-11 19:24:49 -0300

    algolia meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function LandingPage() {
 
   return (
     <main className="w-full h-full">
+      <meta name="algolia-site-verification" content="DA5B16D5E54314AA" />
       {/* Skip to content link for accessibility */}
       <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-background focus:text-foreground focus:outline-none focus:ring-2 focus:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1">
         Skip to content
